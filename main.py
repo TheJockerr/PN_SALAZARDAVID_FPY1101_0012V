e85743dee97ae252a062b7ea295d19b7b9c75b97
TheJockerr <davi.salazarc@duocuc.cl>
2024-07-17 21:10:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
+from statistics import geometric_mean
 trabajadores = [ 'Juan Pérez','María García','Carlos López','Ana Martínez','Pedro Rodríguez','Laura Hernández','Miguel Sánchez','Isabel Gómez','Francisco Díaz', 'Elena Fernández']
 trabajadoresConSaldo = {}
 def menu():
     print('-'*39)
     print('-'*10,'Analisis de datos','-'*10)
     print('-'*39)
+
 def Opuno():
     for i in trabajadores:
         trabajadoresConSaldo[i] = random.randrange(300000,2500000,510)
     print(trabajadores)
     print(trabajadoresConSaldo)
+
 def Opdos(tipo):
     contador=0
     global acumulador
@@ -41,6 +44,26 @@ def Opdos(tipo):
         aa=len(a)
         aa=18-aa
         print(a,' '*aa,'$',b)
+
+def Optres(tipo):
+    saldos=list(trabajadoresConSaldo.values())
+    if tipo==1:
+        a='Sueldo mas alto: '
+        SaldoFinality=max(saldos)
+    if tipo==2:
+        a='Sueldo mas bajo: '
+        SaldoFinality=min(saldos)
+    if tipo==3:
+        a='Promedio de sueldos: '
+        SaldoFinality=round(sum(saldos)/len(saldos))
+    if tipo==4:
+        a='Media geometrica: '
+        try:
+            SaldoFinality=geometric_mean(saldos)
+        except:
+            SaldoFinality=0
+    print('\n- ',a,SaldoFinality)
+def Opcuatro():
     
     
 
@@ -54,3 +77,8 @@ Opdos('a')
 Opdos('b')
 Opdos('c')
 print('\nTOTAL SUELDOS: $', acumulador)
+print('-')
+Optres(1)
+Optres(2)
+Optres(3)
+Optres(4)
